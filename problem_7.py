0e61180ca4b5f00c10ed4f7fef0b35b5694c86a8
H I M A N S H U <119969631+himanshuaryan@users.noreply.github.com>
2024-09-06 03:46:17 +0530

    Create problem_7.py

---
+++ problem_7.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+for i in range(5):
+    right(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(30)
+    forward(100)
+    left(120)
+    forward(100)
+    left(120)
+    forward(100)
+    
+done()
