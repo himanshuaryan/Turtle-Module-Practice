6fa7f03ea671389d988075b8779d501e364c49f3
H I M A N S H U <119969631+himanshuaryan@users.noreply.github.com>
2024-09-06 04:03:27 +0530

    Create SUN.py

---
+++ SUN.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+speed(8)
+for i in range(100):
+  pu()
+  forward(40)
+  pd()
+  left(90)
+  forward(40)
+  left(90)
+  forward(40)
+  left(90)
+  forward(40)
+  left(90)
+  pu()
+  forward(40)
+  pd()
+  left(20)
+
+mainloop()
