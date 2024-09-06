2aefb28e515d6e74928c4ac4e765e7e81bea3857
H I M A N S H U <119969631+himanshuaryan@users.noreply.github.com>
2024-09-06 12:56:19 +0530

    Create problem_11.py

---
+++ problem_11.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+fillcolor("hotpink")
+begin_fill()
+for i in range(20):
+    for a in range(4):
+        forward(40)
+        right(90)
+    forward(80)
+end_fill()
+    
+mainloop()
