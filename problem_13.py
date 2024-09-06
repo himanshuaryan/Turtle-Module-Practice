ec9e7eb5b6e38bfbc44e895d4f1d1d2261c8ed7d
H I M A N S H U <119969631+himanshuaryan@users.noreply.github.com>
2024-09-06 13:28:28 +0530

    Create problem_13.py

---
+++ problem_13.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+bgcolor("yellow")
+pen
+for d in range(3):
+    right(120)
+    forward(180)
+for a in range(3):
+    forward(180)
+    right(120)
+left(60)
+for i in range(3):
+    forward(180)
+    left(120)
+right(120)
+mainloop()
